68e431ca0b438f93f21f96a5e7be184d7ad4106c
Dmitry Zakharov <dzakh.dev@gmail.com>
2020-06-07 11:42:13 +0300

    Format code with prettier

--- test/replace.test.js
+++ test/replace.test.js
@@ -2,26 +2,36 @@ var postcss = require('postcss');
 var replace = require('../lib/replace');
 
 test('Always requires argment', () => {
-  expect(() => {replace()}).toThrow();
+  expect(() => {
+    replace();
+  }).toThrow();
 });
 
 test('Be sure to have "before" and "after" object names', () => {
-  expect(() => {replace({"before": []})}).toThrow();
+  expect(() => {
+    replace({ before: [] });
+  }).toThrow();
 });
 
 test('Objects "before" and "after" must be of type Array', () => {
-  expect(() => {replace({"before": [], "after": 0})}).toThrow();
+  expect(() => {
+    replace({ before: [], after: 0 });
+  }).toThrow();
 });
 
 test('Array length is 1 or more', () => {
-  expect(() => {replace({"before": [], "after": []})}).toThrow();
-  expect(() => {replace({"before": ['[test]'], "after": []})}).toThrow();
+  expect(() => {
+    replace({ before: [], after: [] });
+  }).toThrow();
+  expect(() => {
+    replace({ before: ['[test]'], after: [] });
+  }).toThrow();
 });
 
 test('Replace pattern test', () => {
-  expect(postcss(replace({'before': ['[test]'], 'after': ['^']})).process('h1{}').css)
-    .toBe('h1{}');
+  expect(postcss(replace({ before: ['[test]'], after: ['^'] })).process('h1{}').css).toBe('h1{}');
 
-  expect(postcss(replace({'before': ['[test]'], 'after': ['^']})).process('[test]h1{}').css)
-    .toBe('^h1{}');
+  expect(postcss(replace({ before: ['[test]'], after: ['^'] })).process('[test]h1{}').css).toBe(
+    '^h1{}'
+  );
 });
